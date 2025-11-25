ef66b299e483d9e1c7f2e98591a36ca0d54d47e8
mostlypostly-admin <troy@mostlypostly.com>
2025-11-25 14:33:26 -0500

    Added Manager signup.

--- server.js
+++ server.js
@@ -130,8 +130,6 @@ app.use("/public", express.static(PUBLIC_DIR));
 // =====================================================
 app.use("/manager", managerAuth);
 
-app.use("/manager", require("./src/routes/managerAuth"));
-
 // =====================================================
 // Stylist Portal (token-based, no login)
 // =====================================================
